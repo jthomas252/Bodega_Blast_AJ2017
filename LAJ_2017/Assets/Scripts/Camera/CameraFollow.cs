a2cb2c94466743da8dd85ff3676b012b4e97ce56
Jonathan Thomas <jthom252@gmail.com>
2017-01-02 01:30:01 -0800

    Day 0 Work

---
+++ LAJ_2017/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+    public GameObject followObject; 
+    
+    public Vector3 basePosition; 
+    public Vector3 baseRotation; 
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    this.transform.position = new Vector3(
+            followObject.transform.position.x,
+            followObject.transform.position.y + basePosition.y,
+            followObject.transform.position.z + basePosition.z
+            );
+
+        Quaternion q = followObject.transform.rotation; 
+
+        q.eulerAngles = new Vector3(
+            followObject.transform.rotation.x + baseRotation.x,
+            followObject.transform.rotation.y,
+            this.transform.rotation.z
+            );
+
+        this.transform.rotation = q; 
+	}
+}
