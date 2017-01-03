69e1601086a71f0d02153278ce3cce1d1228246d
Jonathan Thomas <jthom252@gmail.com>
2017-01-02 16:10:51 -0800

    More prefab setup

--- LAJ_2017/Assets/Scripts/Player/Player.cs
+++ LAJ_2017/Assets/Scripts/Player/Player.cs
@@ -2,38 +2,12 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
-    private Rigidbody _rigidbody; 
-
+ 
 	// Use this for initialization
 	void Start () {
-	    _rigidbody = this.GetComponentInChildren<Rigidbody>(); 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        //Move me to InputHandler / callbacks
-        if (Input.GetKey(KeyCode.W)) {
-            _rigidbody.AddForce(this.transform.forward * 1f); 
-        }
-        
-        if (Input.GetKey(KeyCode.A)) {
-            Quaternion q = _rigidbody.rotation;
-            Vector3 v = q.eulerAngles;
-            v.y -= 2.5f;
-            q.eulerAngles = v; 
-            _rigidbody.rotation = q;
-        }
-
-        if (Input.GetKey(KeyCode.S)) {
-            _rigidbody.AddForce(this.transform.forward * -1f);
-        }
-
-        if (Input.GetKey(KeyCode.D)) {
-            Quaternion q = _rigidbody.rotation;
-            Vector3 v = q.eulerAngles;
-            v.y += 2.5f;
-            q.eulerAngles = v;
-            _rigidbody.rotation = q;
-        }
 	}
 }
