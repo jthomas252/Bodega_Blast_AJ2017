75c5fb776d45b238e00ae6ff325507108fe1a3a6
Jonathan Thomas <jthom252@gmail.com>
2017-01-03 22:40:26 -0800

    Day 2 Work: Vehicle working, added ramps, more code skeleton.

--- LAJ_2017/Assets/Scripts/Cart/Wheel.cs
+++ LAJ_2017/Assets/Scripts/Cart/Wheel.cs
@@ -1,21 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-
 namespace LAJ2017 {
     [RequireComponent (typeof (WheelCollider))]
     public class Wheel : MonoBehaviour {
-        private WheelCollider _wheelCollider; 
+        private WheelCollider wheelCollider; 
         public  GameObject    wheelMesh;
+        public  bool          steerable = true; 
 
         public float turningRange = 30f; 
+        public float maxSpeed     = 15f; 
+        public float speedDecay   = 1f;
 
         private void Awake() {
-
+            if (wheelMesh == null) {
+                Debug.LogWarning("Wheel has no mesh attached");
+            }
+            wheelCollider = GetComponent<WheelCollider>(); 
         }
 
-        public void Acclerate() {
+        public void Update() {
+            Debug.Log(wheelCollider.motorTorque);
+            Debug.Log(wheelCollider.steerAngle);
+            wheelMesh.transform.Rotate(new Vector3(wheelCollider.motorTorque, 0f));
+            if (wheelCollider.motorTorque > 0) { 
+                wheelCollider.motorTorque -= (speedDecay * Time.deltaTime);
+            } else if (wheelCollider.motorTorque < -0.1f) {
+                wheelCollider.motorTorque += (speedDecay * Time.deltaTime);
+            }
+        }
 
+        public void Accelerate(float speed) {
+            wheelCollider.motorTorque += speed; 
+            if (wheelCollider.motorTorque > maxSpeed) wheelCollider.motorTorque = maxSpeed; 
+        }
+
+        public void Turn(float speed) {
+            if (steerable) {
+                wheelCollider.steerAngle += speed;
+
+                if (wheelCollider.steerAngle > turningRange) {
+                    wheelCollider.steerAngle = turningRange;
+                } else if (wheelCollider.steerAngle < -turningRange) {
+                    wheelCollider.steerAngle = -turningRange; 
+                }
+
+                Quaternion q = wheelMesh.transform.localRotation;
+                Vector3 v = q.eulerAngles;
+                v.y = wheelCollider.steerAngle;
+                q.eulerAngles = v;
+                wheelMesh.transform.localRotation = q;
+            }
+        }
+
+        public void Brake(float speed) {
+            wheelCollider.motorTorque -= speed;
+            if (wheelCollider.motorTorque < -maxSpeed) wheelCollider.motorTorque = -maxSpeed; 
         }
     }
 }
--- LAJ_2017/Assets/Scripts/Player/PlayerCart.cs
+++ LAJ_2017/Assets/Scripts/Player/PlayerCart.cs
@@ -3,39 +3,41 @@ using System.Collections;
 
 namespace LAJ2017 {
     public class PlayerCart : MonoBehaviour {
-        private Rigidbody _rigidbody;
-        private WheelCollider[] wheels;
+        
+        public float turningSpeed     = 2.5f; 
+        public float accelerationRate = 4f; 
 
+        private Wheel[] wheels;
+
+        
         // Use this for initialization
         void Start() {
-            _rigidbody = this.GetComponentInChildren<Rigidbody>();
-            wheels = this.GetComponentsInChildren<Wheel>();
+            wheels = GetComponentsInChildren<Wheel>();
         }
 
         // Update is called once per frame
         void Update() {
-            //Move me to InputHandler / callbacks
             if (Input.GetKey(KeyCode.W)) {
                 for (int i = 0; i < wheels.Length; ++i) {
-                    wheels[i].Accelerate(acceleration);
+                    wheels[i].Accelerate(accelerationRate * Time.deltaTime);
                 }
             }
 
             if (Input.GetKey(KeyCode.A)) {
                 for (int i = 0; i < wheels.Length; ++i) {
-                    wheels[i].Turn();
+                    wheels[i].Turn(-turningSpeed * Time.deltaTime);
                 }
             }
 
             if (Input.GetKey(KeyCode.S)) {
                 for (int i = 0; i < wheels.Length; ++i) {
-                    wheels[i].Brake(); 
+                    wheels[i].Brake(accelerationRate * Time.deltaTime); 
                 }
             }
 
             if (Input.GetKey(KeyCode.D)) {
                 for (int i = 0; i < wheels.Length; ++i) {
-                    wheels[i].Turn(); 
+                    wheels[i].Turn(turningSpeed * Time.deltaTime); 
                 }
             }
         }
